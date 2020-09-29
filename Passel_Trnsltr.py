00f48e4a532e5d9c0b3fb8bea5a0298b84c293f2
Abhi4793 <54951303+Abhi4793@users.noreply.github.com>
2020-09-29 17:57:06 +0530

    Update Passel_Trnsltr.py

--- Passel_Trnsltr.py
+++ Passel_Trnsltr.py
@@ -1,45 +1,74 @@
 # -*- coding: utf-8 -*-
 """
-Spyder Editor
-
-This is a temporary script file.
+This is part of the ongoing development of Translator UI developed for the purpose of Hackademy2020.
 """
 
 from tkinter import *
-from tkinter.ttk import *
+from tkinter.ttk import Combobox
 from tkinter.filedialog import askopenfilename
-from tkinter.messagebox import messagebox as tkMessageBox
-
+import tkinter.messagebox as tkMessageBox
 
 window = Tk()
 
-label_frame = labelFrame(window, borderwidth=2, width = 10, relief="ridge")
+label_frame = LabelFrame(window, borderwidth=2, width = 10, relief="ridge")
 label_frame.pack(expand='yes', fill='both')
-s = Style()
-s.configure('My.TFrame', background='lightgrey')
 
-var = Stringvar()
+var = StringVar()
 var.set("English(Default)")
 data=( "English","German","French","Spanish","Chinese")
-cb=Combobox(window, width=22, height=50, values=data)
+cb=Combobox(window, width = 22, height = 50, values=data)
 cb.place(x=400,y=35)
 
 label1 = Label(window, borderwidth = 2, width = 10, height = 2, relief="ridge", text="Langguage..")
 label1.place(x=300, y=30)
 
 
-def openwoindow():
+def openNewWindow():
         
     newwindwow = Toplevel(window)
     newWindow.title("Configuration")
     newWindow.geometry("300x150")
     newWindowmaxsize(400, 180)
 
-    label2 = Label
+    label2 = Label(newWindow, text = "Glssary File Location..")
+    label2.place(x=35, y=75) 
+    
+    btn_inner(newWindow, text="Browse", borderwidth = 2, width = 5, height = 2, relief="ridge", side=RIGHT, command=openNewWindow())
+    btn_inner.place(x=35, y=25) 
 
 
+def VideoBrowse():
+    window.destinationdirectory = filedialog.askdirectory(initialdir = "C:\\Users\\Abhishek\\Resources")
+    window.destinationText.insert('1', window.files_list)
+    message.showinfo("Translation In Progress")
 
-mail1 = Frame(window, style='My.TFrame')
-mail1.place(height=70, width=400, x=10, y=10)
-mail1.config()
+
+def DocumentBrowse():
+    window.files_list = list(filedialog.askopenfilenames(initialdir = "C:\\Users\\Abhishek\\Resources"))
+    window.sourceText.insert('1', window.files_list)
+    message.showinfo("Translation In Progress")
+
+
+btn = Button(label_frame, text ="Settings", borderwidth=2, width=28, height=2, relief="ridge", command = openNewWindow)
+btn.place(x=35, y=25)
+
+btn1 = Button(label_frame, text ="Call Trans", borderwidth=1, width=12, height=5, relief="ridge")
+btn1.place(x=35, y=25)
+
+btn2 = Button(label_frame, text ="Chat Trans", borderwidth=1, width=12, height=5, relief="ridge")
+btn2.place(x=35, y=25)
+
+btn3 = Button(label_frame, text ="Video Trans", borderwidth=1, width=12, height=5, relief="ridge", command = VideoBrowse())
+btn3.place(x=35, y=25)
+
+btn4 = Button(label_frame, text ="Document Trans", borderwidth=1, width=12, height=5, relief="ridge", command = DocumentBrowse())
+btn4.place(x=35, y=25)
+
+btn5 = Button(label_frame, text ="Text Trans", borderwidth=1, width=12, height=5, relief="ridge")
+btn5.place(x=35, y=25)
+
+
+window.title('Passel Translator')
+window.geometry("600X200")
+window.maxsize(600, 200)
 window.mainloop()
