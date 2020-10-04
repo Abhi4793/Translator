abf705cab8a8bb59f5f53407e787e82c1a136933
Abhi4793 <54951303+Abhi4793@users.noreply.github.com>
2020-10-04 19:31:50 +0530

    Update Passel_Trnsltr.py

--- Passel_Trnsltr.py
+++ Passel_Trnsltr.py
@@ -3,8 +3,8 @@ from tkinter import *
 from tkinter.ttk import Combobox
 from tkinter.filedialog import askopenfilename
 import tkinter.messagebox as tkMessageBox
-# from PIL import Image
-# from PIL import ImageTk
+from PIL import Image
+from PIL import ImageTk
 
 window = Tk()
 
@@ -18,7 +18,7 @@ data=( "English","German","French","Spanish","Chinese")
 cb=Combobox(window, width=22, height=50, values=data)
 cb.place(x=400,y=35)
 
-label1 = Label(window, borderwidth = 2, width = 10, height = 2, relief="ridge", text="Langguage..")
+label1 = Label(window, borderwidth = 2, width = 10, height = 2, relief="ridge", text="Language..")
 label1.place(x=300, y=30)
 
 def Destinationfolder():
@@ -43,35 +43,54 @@ def openNewWindow():
     # c1 = Tk.Checkbutton(newWindow, text='Disability Mode')
     # c1.place(x=15, y=145)
 
-# icon1 = PhotoImage(file = "C:\\Users\\Abhi\\Desktop\\Icons\\Settings_icon.png")
-# Callicon = PhotoImage(file = "C:\\Users\\Abhi\\Desktop\\Icons\\Callicon.png")
-# Chaticon = PhotoImage(file = "C:\\Users\\Abhi\\Desktop\\Icons\\Chaticon.png")
-# Docicon = PhotoImage(file = "C:\\Users\\Abhi\\Desktop\\Icons\\Docicon.png")
-# Videoicon = PhotoImage(file = "C:\\Users\\Abhi\\Desktop\\Icons\\Videoicon.png")
-# Texticon = PhotoImage(file = "C:\\Users\\Abhi\\Desktop\\Icons\\Texticon.png")
+Settings = PhotoImage(file = "C:\\Users\\Abhishek\\Desktop\\Translator\\Settings.png")
+Callicon = PhotoImage(file = "C:\\Users\\Abhishek\\Desktop\\Translator\\Callicon.png")
+Chaticon = PhotoImage(file = "C:\\Users\\Abhishek\\Desktop\\Translator\\Chaticon.png")
+Docicon = PhotoImage(file = "C:\\Users\\Abhishek\\Desktop\\Translator\\Docicon.png")
+Videoicon = PhotoImage(file = "C:\\Users\\Abhishek\\Desktop\\Translator\\Video_icon.png")
+Texticon = PhotoImage(file = "C:\\Users\\Abhishek\\Desktop\\Translator\\Texticon.png")
+Muteicon = PhotoImage(file="C:\\Users\\Abhishek\\Desktop\\Translator\\Muteicon.png")
+Volumeicon = PhotoImage(file="C:\\Users\\Abhishek\\Desktop\\Translator\\Volumeicon.png")
 
-
-btn = Button(label_frame, text ="Settings", borderwidth=2, width=28, height=2, relief="ridge", command = openNewWindow)
+btn = Button(label_frame, image = Settings, text ="Settings", borderwidth=2, width=200, height=45, relief="ridge", command = openNewWindow)
 btn.place(x=35, y=25)
+
+
+
+    
+def openCallWindow():
+        
+    CallWindow = Toplevel(window)
+    CallWindow.title("Ongoing Call")
+    CallWindow.geometry("300x200")
+    CallWindow.maxsize(300, 200)
+    
+    def Mutemic():
+        Volumebtn.configure(image=Muteicon)
+    
+    Volumebtn = Button(CallWindow, image = Volumeicon, borderwidth=1, width=30, height=30, relief="ridge", command = Mutemic)
+    Volumebtn.place(x=125, y=110)
+    
 
 def ConsentWindow():
         
-    CallWindow = Toplevel(window)
-    CallWindow.title("Consent")
-    CallWindow.geometry("375x100")
-    CallWindow.maxsize(375, 100)
-
-    label2 = Label(CallWindow, text = "This Call will be recorded for further Translation purposes.")
+    ConsentWindow = Toplevel(window)
+    ConsentWindow.title("Consent")
+    ConsentWindow.geometry("375x100")
+    ConsentWindow.maxsize(375, 100)
+        
+    label2 = Label(ConsentWindow, text = "This Call will be recorded for further Translation purposes.")
     label2.place(x=15, y=25) 
-    
-    Continuebutton = Button(CallWindow, text ="Continue", borderwidth=1, width=11, height=1, relief="ridge")
+
+    Continuebutton = Button(ConsentWindow, text ="Continue", borderwidth=1, width=11, height=1, relief="ridge", command=openCallWindow)
     Continuebutton.place(x=170, y=67)
     
-    Declinebutton = Button(CallWindow, text ="Decline", borderwidth=1, width=11, height=1, relief="ridge")
+    Declinebutton = Button(ConsentWindow, text ="Decline", borderwidth=1, width=11, height=1, relief="ridge", command = ConsentWindow.destroy)
     Declinebutton.place(x=280, y=67)
-
-btn1 = Button(label_frame, text ="Call Translator", borderwidth=1, width=13, height=5, relief="ridge", command = ConsentWindow)
-btn1.place(x=22, y=100)
+    
+    
+btn1 = Button(label_frame, image = Callicon, text ="Call Translator", borderwidth=1, width=100, height=95, relief="ridge", command = ConsentWindow)
+btn1.place(x=20, y=85)
 
 #Function to open Chat application & active window selection
 def openchatselection():
@@ -91,15 +110,15 @@ def openchatselection():
     cb1.place(x=135, y=35)
  
     Activelabel = Label(ChatWindow, text = "Select Application")
-    Activelabel.place(x=135, y=75)
+    Activelabel.place(x=25, y=75)
 #This section needs to be dynamic to get active windows from background
     var = StringVar()
     data=("Active Window 1", "Active Window 2")
     cb1=Combobox(ChatWindow, width=20, height=50, values=data)
-    cb1.place(x=25, y=75)
+    cb1.place(x=135, y=75)
 
-btn2 = Button(label_frame, text ="Chat Translator", borderwidth=1, width=13, height=5, relief="ridge", command = openchatselection)
-btn2.place(x=140, y=100)
+btn2 = Button(label_frame, image = Chaticon, text ="Chat Translator", borderwidth=1, width=100, height=95, relief="ridge", command = openchatselection)
+btn2.place(x=140, y=85)
 
 def VideoBrowse():
     
@@ -109,8 +128,8 @@ def VideoBrowse():
     
     messagebox.showinfo("Translation in Progress")
 
-btn3 = Button(label_frame, text ="Video Translator", borderwidth=1, width=13, height=5, relief="ridge", command = VideoBrowse)
-btn3.place(x=255, y=100)
+btn3 = Button(label_frame, image = Videoicon, text ="Video Translator", borderwidth=1, width=100, height=95, relief="ridge", command = VideoBrowse)
+btn3.place(x=255, y=85)
 
 def DocumentBrowse():
     OriginDirectory = filedialog.askdirectory(initialdir = "C:\\Users\\Public\\Desktp")
@@ -119,11 +138,11 @@ def DocumentBrowse():
     
     messagebox.showinfo("Translation in Progress")
     
-btn4 = Button(label_frame, text ="File Translator", borderwidth=1, width=13, height=5, relief="ridge", command = DocumentBrowse)
-btn4.place(x=370, y=100)
+btn4 = Button(label_frame, image = Docicon, text ="File Translator", borderwidth=1, width=100, height=95, relief="ridge", command = DocumentBrowse)
+btn4.place(x=370, y=85)
 
-btn5 = Button(label_frame, text ="Text Translator", borderwidth=1, width=13, height=5, relief="ridge")
-btn5.place(x=485, y=100)
+btn5 = Button(label_frame, image = Texticon, text ="Text Translator", borderwidth=1, width=100, height=95, relief="ridge")
+btn5.place(x=485, y=85)
 
 
 window.title("meetTranslate")
