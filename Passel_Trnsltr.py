d1c3a12d369c2b4dfa05455bee939b6b75d25d77
Abhishek Mishra <54951303+Abhi4793@users.noreply.github.com>
2020-10-05 09:55:47 +0530

    Added Changes for Video Translation

--- Passel_Trnsltr.py
+++ Passel_Trnsltr.py
@@ -1,4 +1,3 @@
-
 from tkinter import *
 from tkinter import filedialog
 from tkinter.ttk import Combobox
@@ -11,7 +10,13 @@ import os
 import pandas as pd
 import numpy as np
 import PyPDF2
-from fpdf import FPDF 
+from pydub.silence import split_on_silence
+import moviepy.editor as mp
+import speech_recognition as sr
+from pydub import AudioSegment
+from gtts import gTTS
+
+#from fpdf import FPDF
 
 translator = Translator()
 window = Tk()
@@ -31,23 +36,23 @@ label1.place(x=300, y=30)
 
 def Destinationfolder():
     Directory = filedialog.askdirectory(initialdir = "C:\\Users\\Public")
-    
+   
     window.OriginText.insert( '1', Directory)
 
 
 def openNewWindow():
-        
+       
     newWindow = Toplevel(window)
     newWindow.title("Configuration")
     newWindow.geometry("300x200")
     newWindow.maxsize(300, 200)
 
     label2 = Label(newWindow, text = "Translated Resources Default Path..")
-    label2.place(x=15, y=45) 
-    
+    label2.place(x=15, y=45)
+   
     browsebtn = Button(newWindow, text ="Browse", borderwidth=1, width=11, height=2, relief="ridge", command = Destinationfolder)
     browsebtn.place(x=210, y=37)
-    
+   
     # c1 = Tk.Checkbutton(newWindow, text='Disability Mode')
     # c1.place(x=15, y=145)
 
@@ -65,44 +70,44 @@ btn.place(x=35, y=25)
 
 
 
-    
+   
 def openCallWindow():
-        
+       
     CallWindow = Toplevel(window)
     CallWindow.title("Ongoing Call")
     CallWindow.geometry("300x200")
     CallWindow.maxsize(300, 200)
-    
+   
     def Mutemic():
         Volumebtn.configure(image=Muteicon)
-    
+   
     Volumebtn = Button(CallWindow, image = Volumeicon, borderwidth=1, width=30, height=30, relief="ridge", command = Mutemic)
     Volumebtn.place(x=125, y=110)
-    
+   
 
 def ConsentWindow():
-        
+       
     ConsentWindow = Toplevel(window)
     ConsentWindow.title("Consent")
     ConsentWindow.geometry("375x100")
     ConsentWindow.maxsize(375, 100)
-        
+       
     label2 = Label(ConsentWindow, text = "This Call will be recorded for further Translation purposes.")
-    label2.place(x=15, y=25) 
+    label2.place(x=15, y=25)
 
     Continuebutton = Button(ConsentWindow, text ="Continue", borderwidth=1, width=11, height=1, relief="ridge", command=openCallWindow)
     Continuebutton.place(x=170, y=67)
-    
+   
     Declinebutton = Button(ConsentWindow, text ="Decline", borderwidth=1, width=11, height=1, relief="ridge", command = ConsentWindow.destroy)
     Declinebutton.place(x=280, y=67)
-    
-    
+   
+   
 btn1 = Button(label_frame, image = Callicon, text ="Call Translator", borderwidth=1, width=100, height=95, relief="ridge", command = ConsentWindow)
 btn1.place(x=20, y=85)
 
 #Function to open Chat application & active window selection
 def openchatselection():
-    
+   
     ChatWindow = Toplevel(window)
     ChatWindow.title("Chat Selection")
     ChatWindow.geometry("300x150")
@@ -130,19 +135,107 @@ btn2.place(x=140, y=85)
 
 def VideoBrowse():
 
-    window.OriginDirectory = list(filedialog.askopenfilenames(initialdir = "C:\\Users\\Riyaz\\Desktop"))
-    
-    window.SourceText.insert( '1', window.OriginDirectory)
-    
-    messagebox.showinfo("Translation in Progress")
-        
+    filename = filedialog.askopenfilename(initialdir = "C:\\Users\\Abhishek\\Desktop\\aToV")
+   
+    clip = mp.VideoFileClip(filename)
+    clip.audio.write_audiofile(r"Test_audio.wav")
+   
+    r = sr.Recognizer()
+   
+    # a function that splits the audio file into chunks
+    # and applies speech recognition
+    def get_large_audio_transcription(path):
+        """
+        Splitting the large audio file into chunks
+        and apply speech recognition on each of these chunks
+        """
+        # open the audio file using pydub
+        sound = AudioSegment.from_wav(path)  
+        # split audio sound where silence is 700 miliseconds or more and get chunks
+        chunks = split_on_silence(sound,
+            # experiment with this value for your target audio file
+            min_silence_len = 500,
+            # adjust this per requirement
+            silence_thresh = sound.dBFS-14,
+            # keep the silence for 1 second, adjustable as well
+            keep_silence=500,
+        )
+        folder_name = "audio-chunks"
+        # create a directory to store the audio chunks
+        if not os.path.isdir(folder_name):
+            os.mkdir(folder_name)
+        whole_text = ""
+        # process each chunk
+        for i, audio_chunk in enumerate(chunks, start=1):
+            # export audio chunk and save it in
+            # the `folder_name` directory.
+            chunk_filename = os.path.join(folder_name, f"chunk{i}.wav")
+            audio_chunk.export(chunk_filename, format="wav")
+            # recognize the chunk
+            with sr.AudioFile(chunk_filename) as source:
+                audio_listened = r.record(source)
+                # try converting it to text
+                try:
+                   
+                    text = r.recognize_google(audio_listened)
+                except sr.UnknownValueError as e:
+                    print("Error: Could be music", str(e))
+                else:
+                    text = f"{text.capitalize()}. "
+                    print(chunk_filename, ":", text)
+                    whole_text += text
+        # return the text for all chunks detected
+        return whole_text
+   
+   
+    def doc_trans():
+        f = open('text.txt', 'r')
+        contents = f.read()
+        print(contents)
+        file_translate = Translator()
+        result = file_translate.translate(contents, dest='fr')
+        print(result.text)
+        with open('textTranslated.txt', 'w',encoding="utf-8") as f:
+            f.write(result.text)
+   
+   
+    def combine_audio(vidname, audname, outname, fps=25):
+        import moviepy.editor as mpe
+        my_clip = mpe.VideoFileClip(vidname)
+        audio_background = mpe.AudioFileClip(audname)
+        final_clip = my_clip.set_audio(audio_background)
+        final_clip.write_videofile(outname,fps)
+        os.system(outname)
+   
+    def textToSpeech(filename):
+        file = open(filename, "r",encoding="utf-8").read().replace("\n", " ")
+        language = 'fr'
+        speech = gTTS(text = str(file), lang = language, slow = False)
+        speech.save("voice.mp3")
+        #os.system("start voice.mp3")
+   
+    path = "Test_audio.wav"
+    full_Text=get_large_audio_transcription(path)
+    with open("text.txt","w+") as file:
+        file.writelines(full_Text)
+    doc_trans()
+    textToSpeech("textTranslated.txt")
+    combine_audio("TestAudio.mp4","voice.mp3","Merged_out_final.mp4")
+    messagebox.showinfo("Showinfo","File Saved")
+    # window.OriginDirectory = list(filedialog.askopenfilenames(initialdir = "C:\\Users\\Riyaz\\Desktop"))
+   
+    # window.SourceText.insert( '1', window.OriginDirectory)
+   
+    # messagebox.showinfo("Translation in Progress")
+       
     #messagebox.showinfo("Translation in Progress")
 
 btn3 = Button(label_frame, image = Videoicon, text ="Video Translator", borderwidth=1, width=100, height=95, relief="ridge", command = VideoBrowse)
 btn3.place(x=255, y=85)
 
+
 def DocumentBrowse():
-    filename = filedialog.askopenfilename(initialdir = "C:\\Users\\Riyaz\\Desktop")
+    filename = filedialog.askopenfilename(initialdir = "C:\\Users\\Abhishek\\Desktop")
     if os.path.splitext(filename)[1] == ".txt" :
         file1 = open(filename,"r")
         text_to_translate = file1.read()
@@ -163,7 +256,7 @@ def DocumentBrowse():
 
         for cols in np.array(df.columns):
             df[cols] = df[cols].apply(translate)
-        
+       
         for i in range(df.shape[1]):
             cols_new.append(translate(df.columns[i]))
         df.columns = cols_new
@@ -174,7 +267,7 @@ def DocumentBrowse():
         pdfFileObj = open(filename, 'rb')
         pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
         file2 = open(os.path.splitext(filename)[0]+"_translated.txt","w")
-        
+       
         for i in range(pdfReader.numPages):
             pageObj = pdfReader.getPage(i)
             text = pageObj.extractText()
@@ -188,8 +281,8 @@ def DocumentBrowse():
                 file2.write(text_translated.text)
             file2.write("\n---------------END OF PAGE--------------\n")
         file2.close()
-    #messagebox.showinfo("File Saved")
-    
+    messagebox.showinfo("Showinfo","File Saved")
+   
 btn4 = Button(label_frame, image = Docicon, text ="File Translator", borderwidth=1, width=100, height=95, relief="ridge", command = DocumentBrowse)
 btn4.place(x=370, y=85)
 
